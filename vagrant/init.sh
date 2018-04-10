77a82deeeedd5dff7cce0c1cc52535f08ab64783
shunyi <M157q.tw@gmail.com>
2018-04-10 18:59:39 +0800

    Add vagrant related settings

---
+++ vagrant/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Dependencies for building handbook
+pkg install -y docproj subversion
+
+# For running simple http server
+pkg install -y python3
+
+# Clone source code for using Makefile to build handbook
+svn checkout https://svn.FreeBSD.org/doc/head /home/vagrant/doc
+
+# Change permissions for vagrant normal user
+chown -R vagrant /home/vagrant/doc
